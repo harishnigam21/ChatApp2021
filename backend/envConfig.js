48a738dfe43f9594cf42404a9115e926d4e8bc35
Harish Nigam <harishnigam21@gmail.com>
2026-02-26 11:32:46 +0530

    adding env config file

---
+++ backend/envConfig.js
@@ -0,0 +1,18 @@
+import { configDotenv } from "dotenv";
+configDotenv();
+
+export const envList = {
+  PORT: process.env.PORT,
+  BACKEND_HOST: process.env.BACKEND_HOST,
+  FRONTEND_HOST: process.env.FRONTEND_HOST,
+  ACCESS_TOKEN_KEY: process.env.ACCESS_TOKEN_KEY,
+  REFRESH_TOKEN_KEY: process.env.REFRESH_TOKEN_KEY,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  DB_STRING: process.env.DB_STRING,
+  MAX_IMAGE_SIZE: process.env.MAX_IMAGE_SIZE,
+  IMAGE_FORMAT_ALLOWED: process.env.IMAGE_FORMAT_ALLOWED,
+  MAX_VIDEO_SIZE: process.env.MAX_VIDEO_SIZE,
+  VIDEO_FORMAT_ALLOWED: process.env.VIDEO_FORMAT_ALLOWED,
+};
---
+++ frontend/envConfig.js
@@ -0,0 +1,4 @@
+export const envList = {
+  BACKEND_HOST: import.meta.env.VITE_BACKEND_HOST,
+  FRONTEND_HOST: import.meta.env.VITE_FRONTEND_HOST,
+};
