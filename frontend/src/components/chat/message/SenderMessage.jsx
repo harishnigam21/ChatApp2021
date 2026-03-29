fddfb9ef7b6100f4527871ca61e2a995a216bea6
Harish Nigam <harishnigam21@gmail.com>
2026-03-29 09:04:28 +0530

    Update SenderMessage.jsx
    adding checkbox to each message to build message selection functionality

--- frontend/src/components/chat/message/SenderMessage.jsx
+++ frontend/src/components/chat/message/SenderMessage.jsx
@@ -1,26 +1,71 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { media } from "../../../assets/data/media";
 import { separateTime } from "../../../utils/getDate";
 import ImageLoading from "./ImageLoading";
-
-export default function SenderMessage({ msg,selectedUser }) {
+import { useState } from "react";
+import {
+  popMessageToDelete,
+  pushMessageToDelete,
+} from "../../../redux/slices/SelectedUserSlice";
+export default function SenderMessage({ msg, selectedUser }) {
+  const dispatch = useDispatch();
+  const [checked, setChecked] = useState(false);
   const onlineUsers = useSelector((store) => store.user.onlineUsers);
+  const user = useSelector((store) => store.user.userInfo);
+  const handleCheck = async (e, id) => {
+    const result = e.target.checked;
+    if (result) {
+      dispatch(pushMessageToDelete(id));
+    } else {
+      dispatch(popMessageToDelete(id));
+    }
+    setChecked(result);
+  };
+  if (msg.deletedFor.includes(user._id)) {
+    return;
+  }
   return (
-    <section className=" rounded-[0px_10px_10px_10px] flex flex-col bg-secondary/80 text-black min-w-30 max-w-[50%] wrap-anywhere you self-start overflow-hidden">
-     {msg.image && <ImageLoading msg={msg}/>}
-      <div className="relative pb-3 flex gap-2">
-        <p className="wrap-anywhere p-1 pl-2">{msg.message}</p>
-        <small className="absolute bottom-0 right-0 text-[10px] grow text-right flex flex-nowrap items-end justify-end gap-1 whitespace-nowrap">
-          {separateTime(msg.createdAt)}
-          {msg.seen ? (
-            <media.BiCheckDouble className="text-base text-blue-700" />
-          ) : onlineUsers?.includes(selectedUser._id) ? (
-            <media.BiCheckDouble className="text-base" />
-          ) : (
-            <media.BiCheck className="text-base" />
-          )}
-        </small>
-      </div>
+    <section
+      className={`flex gap-2 ${checked && "bg-gray-600/30"} p-1 rounded-md transition-all`}
+    >
+      <label
+        htmlFor={`msg/${msg._id}`}
+        className=" rounded-[0px_10px_10px_10px] flex flex-col bg-secondary/80 text-black min-w-30 max-w-[50%] wrap-anywhere you self-start overflow-hidden"
+      >
+        {msg.deletedForEveryone ? (
+          <div className="flex items-center gap-2 wrap-anywhere p-1 pl-2">
+            <media.IoBanOutline className="self-start mt-0.5" />{" "}
+            <span className="text-gray-800/80 font-light text-sm">
+              You deleted this message...
+            </span>
+          </div>
+        ) : (
+          <>
+            {msg.image && <ImageLoading msg={msg} />}
+            <div className="relative pb-3 flex gap-2">
+              <p className="wrap-anywhere p-1 pl-2">{msg.message}</p>
+              <small className="absolute bottom-0 right-0 text-[10px] grow text-right flex flex-nowrap items-end justify-end gap-1 whitespace-nowrap">
+                {separateTime(msg.createdAt)}
+                {msg.seen ? (
+                  <media.BiCheckDouble className="text-base text-blue-700" />
+                ) : onlineUsers?.includes(selectedUser._id) ? (
+                  <media.BiCheckDouble className="text-base" />
+                ) : (
+                  <media.BiCheck className="text-base" />
+                )}
+              </small>
+            </div>
+          </>
+        )}
+      </label>
+      <input
+        checked={checked}
+        onChange={(e) => handleCheck(e, msg._id)}
+        type="checkbox"
+        name="message"
+        className="self-start hidden"
+        id={`msg/${msg._id}`}
+      />
     </section>
   );
 }
